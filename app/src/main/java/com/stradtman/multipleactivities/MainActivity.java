67a9bc0cfc3f87092b4580112e43250004f9f795
Kevin <stradtkt22@gmail.com>
2019-08-31 00:49:40 -0400

    Initial commit

---
+++ app/src/main/java/com/stradtman/multipleactivities/MainActivity.java
@@ -0,0 +1,22 @@
+package com.stradtman.multipleactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void jumpToSecondActivity(View view) {
+        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
